0749f5eea78ccea71957c4c828659f522f66eed7
Niklas Wulf <niklas@wulf-home.de>
2014-01-08 08:57:20 +0100

    Squashed 'INAppStoreWindow/' content from commit 56c34dd
    git-subtree-dir: INAppStoreWindow
    git-subtree-split: 56c34dde36c8361d9d8743c6ccf2ced34c9716e4

---
+++ SampleApp/SampleApp/SampleAppAppDelegate.h
@@ -0,0 +1,34 @@
+//
+//  SampleAppAppDelegate.h
+//  SampleApp
+//
+//  Created by Indragie Karunaratne on 11-02-23.
+//  Copyright 2011 Indragie Karunaratne. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "INAppStoreWindow.h"
+
+@interface SampleAppAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSPanel *sheet;
+@property (assign) IBOutlet INAppStoreWindow *window;
+@property (assign) IBOutlet NSButton *centerFullScreen;
+@property (assign) IBOutlet NSButton *centerTrafficLight;
+@property (assign) IBOutlet NSButton *verticalTrafficLight;
+@property (assign) IBOutlet NSButton *verticallyCenterTitle;
+@property (assign) IBOutlet NSSlider *fullScreenRightMarginSlider;
+@property (assign) IBOutlet NSSlider *trafficLightLeftMargin;
+@property (assign) IBOutlet NSSlider *trafficLightSeparation;
+@property (assign) IBOutlet NSSlider *titleBarHeight;
+@property (assign) IBOutlet NSButton *showsBaselineSeparator;
+@property (nonatomic, retain) NSMutableArray *windowControllers;
+
+- (IBAction)createWindowController:(id)sender;
+- (IBAction)checkboxAction:(id)sender;
+- (IBAction)sliderAction:(id)sender;
+
+- (IBAction)showSheetAction:(id)sender;
+- (IBAction)doneSheetAction:(id)sender;
+
+@end
